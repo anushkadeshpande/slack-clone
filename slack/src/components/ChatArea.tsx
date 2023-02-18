60636d7a33b7fc73295de9f2c3746df73107d388
anushkadeshpande <anushkaa5000@gmail.com>
2023-02-19 02:52:29 +0530

    refactor: Added ChatAreaInput component

--- slack/src/components/ChatArea.tsx
+++ slack/src/components/ChatArea.tsx
@@ -1,25 +1,15 @@
-import {useState} from 'react'
-import './ChatArea.css'
+import { useState } from "react";
+import "./ChatArea.css";
+import ChatAreaInput from "./ChatAreaInput";
 
 const ChatArea = () => {
-
-    const [chatInput, setChatInput] = useState("");
   return (
     <div className="ChatArea">
-        {/* Message bubbles */}
-        
-        <div className="ChatArea__input">
-            <input type="textarea" value={chatInput} onChange={(e) => {setChatInput(e.target.value); console.log(chatInput) }}/>
+      {/* Message bubbles */}
 
-            <button id="send">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-  <path d="M3.478 2.405a.75.75 0 00-.926.94l2.432 7.905H13.5a.75.75 0 010 1.5H4.984l-2.432 7.905a.75.75 0 00.926.94 60.519 60.519 0 0018.445-8.986.75.75 0 000-1.218A60.517 60.517 0 003.478 2.405z" />
-</svg>
-
-            </button>
-        </div>
+      <ChatAreaInput />
     </div>
-  )
-}
+  );
+};
 
-export default ChatArea
+export default ChatArea;
--- slack/src/components/SideBar.tsx
+++ slack/src/components/SideBar.tsx
@@ -67,8 +67,7 @@ const SideBar = () => {
             strokeWidth={8}
             stroke="currentColor"
             className="w-6 h-6"
-            style={{height: "10px",
-                width: "10px"}}
+            style={{ height: "10px", width: "10px" }}
           >
             <path
               strokeLinecap="round"
@@ -137,10 +136,21 @@ const SideBar = () => {
         </span>
 
         <span id="addChannelButton">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-</svg>
-<p>Add Channels</p>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 4.5v15m7.5-7.5h-15"
+            />
+          </svg>
+          <p>Add Channels</p>
         </span>
       </div>
     </div>
