85260903c1cca4922f6aa784245344eae2488646
anushkadeshpande <anushkaa5000@gmail.com>
2023-03-25 02:37:07 +0530

    feat: added redux state persistence

--- slack/src/app/store.js
+++ slack/src/app/store.js
@@ -1,9 +1,33 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import userReducer from '../features/userSlice';
 import menuReducer from '../features/menuSlice';
-export default configureStore({
-  reducer: {
-    user: userReducer,
-    menu: menuReducer
-  },
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import thunk from 'redux-thunk';
+// import storageSession from 'reduxjs-toolkit-persist/lib/storage/session'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+// const menuPersistConfig = {
+//   key: 'menu',
+//   storage: storageSession,
+// }
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  // menu: persistReducer(menuPersistConfig, menuReducer)
+  menu: menuReducer
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: [thunk]
 });
+
+export const persistor = persistStore(store)
